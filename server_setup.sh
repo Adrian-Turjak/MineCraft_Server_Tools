821c8910ccd809bd9e50ffc576dcaf2f4c6b303e
Adrian Turjak <adriant@catalyst.net.nz>
2016-04-03 02:11:13 +1300

    initial push of scripts

---
+++ server_setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# first lets ensure java is at the latest version
+echo "setting up java"
+sudo sudo apt-get --assume-yes purge openjdk-\*
+sudo apt-get --assume-yes install
+sudo add-apt-repository --assume-yes ppa:webupd8team/java
+sudo apt-get --assume-yes update
+sudo apt-get install --assume-yes oracle-java8-installer
+echo "java setup completed"
+
+
+# SET THESE VARIABLES FIRST!
+server_url="http://ftb.cursecdn.com/FTB2/modpacks/FTBInfinity/2_4_2/FTBInfinityServer.zip"
+zip_file="FTBInfinityServer.zip"
+
+echo "getting server files"
+wget $server_url
+unzip $zip_file -d mc_server
+
+echo "Running FTBInstall"
+cd mc_server_new
+. ./FTBInstall.sh
+cd ..
+echo "FTBInstall completed"
+
+echo "Updating ServerStart.sh script"
+# Update this line to the amount of RAM you want the server to use.
+find mc_server/ServerStart.sh -type f -exec sed -i 's/-Xms512M -Xmx2048M/-Xms1000M -Xmx3500M/g' {} \;
+echo "ServerStart.sh script updated"
+
+echo "Setting EULA to true"
+find mc_server/eula.txt -type f -exec sed -i 's/eula=false/eula=true/g' {} \;
+echo "EULA updated"
+
+echo "setting up server as service"
+server_path=$(readlink -f mc_server/ServerStart.sh)
+sudo cp scripts/minecraft-server.conf /etc/init/minecraft-server.conf
+sudo find /etc/init/minecraft-server.conf -type f -exec sudo sed -i 's/<server_path>/${server_path}/g' {} \;
+sudo initctl reload-configuration
+echo "server setup as service"
+
+echo "Starting minecraft server"
+sudo start minecraft-server
+echo "Minecraft server started"
+
+# now setup the cronjob for monitoring.
+# TODO: CRONJOB AND MONITORING STUFF GOES HERE:
---
+++ server_update.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# SET THESE VARIABLES FIRST!
+server_url="http://ftb.cursecdn.com/FTB2/modpacks/FTBInfinity/2_4_2/FTBInfinityServer.zip"
+zip_file="FTBInfinityServer.zip"
+
+wget $server_url
+unzip $zip_file -d mc_server_new
+
+echo "Running FTBInstall"
+cd mc_server_new
+. ./FTBInstall.sh
+cd ..
+echo "FTBInstall completed"
+
+echo "Stopping minecraft server"
+sudo stop minecraft-server
+echo "Minecraft server stopped"
+
+echo "Updating ServerStart.sh script"
+# Update this line to the amount of RAM you want the server to use.
+find mc_server_new/ServerStart.sh -type f -exec sed -i 's/-Xms512M -Xmx2048M/-Xms1000M -Xmx7500M -Dfml.queryResult=confirm/g' {} \;
+echo "ServerStart.sh script updated"
+
+echo "Copying world data over"
+cp -r mc_server/world mc_server_new
+echo "World data copied"
+
+echo "Copying backups over"
+cp -r mc_server/world mc_server_new
+echo "Backups copied"
+
+echo "Copying server properties"
+cp mc_server/server.properties mc_server_new
+echo "Server properties copied"
+
+echo "Copying ops users"
+cp mc_server/ops.json mc_server_new
+echo "Ops users copied"
+
+echo "Copying banned player"
+cp  mc_server/banned-players.json mc_server_new
+echo "Banned players copied"
+
+echo "Copying banned ips"
+cp mc_server/banned-ips.json mc_server_new
+echo "Banned ips copied"
+
+echo "Setting EULA to true"
+find mc_server_new/eula.txt -type f -exec sed -i 's/eula=false/eula=true/g' {} \;
+echo "EULA updated"
+
+echo "swapping folders"
+mv mc_server mc_server_old
+mv mc_server_new mc_server
+echo "folders swapped"
+
+echo "Starting minecraft server"
+sudo start minecraft-server
+echo "Minecraft server started"
